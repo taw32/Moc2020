c1c04726e1a78a13630c797bc559d164dd431b89
taw32 <69284354+taw32@users.noreply.github.com>
2020-08-13 13:33:44 -0400

    Update timing_matmul.py

--- timing_matmul.py
+++ timing_matmul.py
@@ -1,3 +1,4 @@
+
 from scipy import matmul, rand, loadtxt
 
 from time import perf_counter
@@ -5,8 +6,8 @@ import matplotlib.pyplot as plt
 
 tiempo = []
 memoria = []
-N = [1,2,50]#,100,200,500,1000,2000,5000]#,10000,20000]
-Ncorridas = 1
+N = [1,2,5,10,12,15,20,25,30,40,45,50,60,100,200,500,1000,2000,5000]#,10000,20000]
+Ncorridas = 10
 
 for i in range(Ncorridas):
     archivo = (f"matmul{i}.txt")
@@ -35,9 +36,17 @@ for i in range(Ncorridas):
 from time import perf_counter
 import matplotlib.pyplot as plt
 
-Ncorridas = 2
+Ncorridas = 10
 
 plt.figure()
+x = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
+xlab = ["10","20","50","100","200","500","1000","2000","5000","10000","20000"]
+y = [0.1e-3,1e-3,1e-2,0.1,1.,10.,60,60*10, 60*100]
+ylab = ["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min","10 min"] 
+y2 = [10**3,10**4,10**5,10**6,10**7,10**8,10**9,10**10]
+ylab2 = ["1 KB","10 KB","100 KB","1 MB","10 MB","100 MB","1 GB","10 GB","100 GB"]
+limite1 = [0.1e-4,20000,1e-2,6000]
+limite2 = [0.1e-4,20000,1e-2,10000000000]
 #tiempo
 plt.title("Rendimiento A@B")
 for i in range(Ncorridas):
@@ -46,45 +55,23 @@ for i in range(Ncorridas):
     tiempo_lista = datos[:,1]
     n_lista= datos[:,0]
     mem_lista = datos[:,2]      
-    
-    
+    plt.subplot(2,1,1)
+    plt.title("Rendimiento A@B")
+    plt.loglog (n_lista,tiempo_lista,"-o")  
+    plt.xticks(x,[])
+    plt.yticks(y,ylab)
+    plt.grid(True) 
+    plt.ylabel("Tiempo Transcurrido")
 
-
-
-#graficos
-
-x = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
-xlab = ["10","20","50","100","200","500","1000","2000","5000","10000","20000"]
-y = [0.1e-3,1e-3,1e-2,0.1,1.,10.,60,60*10, 60*100]
-ylab = ["0.1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min","10 min"] 
-y2 = [10**3,10**4,10**5,10**6,10**7,10**8,10**9,10**10]
-ylab2 = ["1 KB","10 KB","100 KB","1 MB","10 MB","100 MB","1 GB","10 GB","100 GB"]
-
-
-
-#-----------------------------------------------------------
-plt.subplot(2,1,1)
-plt.title("Rendimiento A@B")
-plt.loglog (n_lista,tiempo_lista,"-o")  
-plt.xticks(x,[],rotation="45")
-plt.yticks(y,ylab)
-plt.grid(True) 
-plt.ylabel("Tiempo Transcurrido (s) ")
-plt.axis([0,20000,0,6000])
-
-#------------------------------------------------------------
 plt.subplot(2,1,2)
-plt.loglog(n_lista,tiempo_lista,"-o")  
-plt.xticks(x,xlab,rotation='45')
+plt.loglog(n_lista,mem_lista,"-o")  
+plt.xticks(x,xlab,rotation=45)
 plt.yticks(y2,ylab2)
 plt.grid(True) 
-plt.ylabel("Uso Memoria (bytes)")
+plt.ylabel("Uso Memoria")
 plt.xlabel("Tamaño de la matriz")
-plt.axis([0,20000,0,10000000000])
 
-
-
-plt.axhline(y = 10000000000, xmin=0.001, xmax=0.99, color="black", ls="--")
-
+plt.axhline(4e9)
+plt.tight_layout()
 plt.savefig("grafico.png")
 plt.show()
